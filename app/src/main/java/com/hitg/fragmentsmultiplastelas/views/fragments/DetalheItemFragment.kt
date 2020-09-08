841289fef684bf5b77dfdd7834807786afdcb323
Helton Isac <helton.isac@gmail.com>
2020-09-08 20:45:58 -0300

    Implementing Fragments

--- app/src/main/java/com/hitg/fragmentsmultiplastelas/views/fragments/DetalheItemFragment.kt
+++ app/src/main/java/com/hitg/fragmentsmultiplastelas/views/fragments/DetalheItemFragment.kt
@@ -4,17 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.hitg.fragmentsmultiplastelas.R
+import com.hitg.fragmentsmultiplastelas.models.Item
 
 class DetalheItemFragment : Fragment() {
+    private var item: Item? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        item = arguments?.getParcelable("item")
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detalhe_item, container, false)
+        val view = inflater.inflate(
+            R.layout.fragment_detalhe_item,
+            container, false
+        )
+        val tvTitle = view.findViewById(R.id.tvTitulo) as TextView
+        val tvBody = view.findViewById(R.id.tvSubtitulo) as TextView
+        tvTitle.text = item?.title
+        tvBody.text = item?.body
+        return view
     }
-
 }
--- app/src/main/java/com/hitg/fragmentsmultiplastelas/views/fragments/ListaItensFragment.kt
+++ app/src/main/java/com/hitg/fragmentsmultiplastelas/views/fragments/ListaItensFragment.kt
@@ -1,20 +1,60 @@
 package com.hitg.fragmentsmultiplastelas.views.fragments
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.fragment.app.Fragment
 import com.hitg.fragmentsmultiplastelas.R
+import com.hitg.fragmentsmultiplastelas.models.Item
+import com.hitg.fragmentsmultiplastelas.views.listeners.OnItemSelectedListener
 
 class ListaItensFragment : Fragment() {
+    private lateinit var adapterItems: ArrayAdapter<Item>
+    private var lvItems: ListView? = null
+    private lateinit var listener: OnItemSelectedListener
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        val myActivity: Activity
+        if (context is Activity) {
+            myActivity = context
+            if (myActivity is OnItemSelectedListener) {
+                listener = myActivity
+            } else {
+                throw ClassCastException(
+                    activity.toString()
+                            + " must implement ItemsListFragment.OnItemSelectedListener"
+                )
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val items = Item.fetchAll()
+        adapterItems = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_list_item_activated_1, items
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lista_itens, container, false)
+        val view = inflater.inflate(R.layout.fragment_lista_itens, container, false)
+        lvItems = view.findViewById(R.id.lvItens)
+        lvItems?.adapter = adapterItems
+        lvItems?.onItemClickListener =
+            AdapterView.OnItemClickListener { adapterView, item, position, rowId ->
+                val i = adapterItems.getItem(position)
+                listener.onItemSelected(i)
+            }
+        return view
     }
-
 }
