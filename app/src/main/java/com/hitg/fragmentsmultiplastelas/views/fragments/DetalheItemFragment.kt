945f10b4ba830611ce59655449f6820d37e73f29
Helton Isac <helton.isac@gmail.com>
2020-09-08 20:35:09 -0300

    Creating Fragments

---
+++ app/src/main/java/com/hitg/fragmentsmultiplastelas/views/fragments/DetalheItemFragment.kt
@@ -0,0 +1,20 @@
+package com.hitg.fragmentsmultiplastelas.views.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hitg.fragmentsmultiplastelas.R
+
+class DetalheItemFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detalhe_item, container, false)
+    }
+
+}
